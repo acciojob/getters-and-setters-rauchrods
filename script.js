75129e006908683ac0423270e883664946148b3e
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-04-09 10:27:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
 //complete this code
-class Person {}
+class Person {
+	    constructor(name, age){
+        this.name=name;
+        this.age=age;       
+    }
 
-class Student extends Person {}
+    get_name() {
+        console.log(this.name);
+    }
+    get_age() {
+        console.log(this.age);
+    }
 
-class Teacher extends Person {}
+    set_name(name) {
+        this.name=name;
+        console.log(this.name);
+    }
+    set_age(age) {
+        this.age=age;
+        console.log(this.age);
+    }
+}
+
+class Student extends Person {
+	   constructor(name, age){
+        super(name,age);     
+    }
+
+   
+    study(){
+        console.log(this.name + " is studying");
+    }
+}
+
+class Teacher extends Person {
+	 constructor(name, age){
+        super(name,age);     
+    }
+    teach(){
+        console.log(this.name + " is teaching");
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
